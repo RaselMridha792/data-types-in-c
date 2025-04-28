03ec456b52ebe43ebeb170cec0adf6460e56cf17
RaselMridha792 <raselmridha792@gmail.com>
2025-04-28 18:00:45 +0600

    explores datatypes in c, float, int, double, string

---
+++ data-type.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+      int a = 10;
+      double x = 10.2243;
+      float y = 19.2233f;
+      printf("%d""\t" "%.2lf\t" "%f", a, x, y); // print only two digit after a double
+    return 0;
+}
