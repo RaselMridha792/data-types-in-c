0c6da83cdcc8699ab72b2fcb63a503b83dddfee3
RaselMridha792 <raselmridha792@gmail.com>
2025-04-28 18:06:06 +0600

    learn string and char in c

--- data-type.c
+++ data-type.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 int main () {
-      int a = 10;
-      double x = 10.2243;
-      float y = 19.2233f;
-      printf("%d""\t" "%.2lf\t" "%f", a, x, y); // print only two digit after a double
+      // int a = 10;
+      // double x = 10.2243;
+      // float y = 19.2233f;
+      // printf("%d""\t" "%.2lf\t" "%f", a, x, y); // print only two digit after a double
+
+      // string and char in c
+      char ch = 's';              // একটি অক্ষর সংরক্ষণ
+      char str[] = "rasel mridha";        // একটি স্ট্রিং (character array)
+  
+      printf("Character: %c\n", ch);
+      printf("String: %s\n", str);
+
     return 0;
 }
