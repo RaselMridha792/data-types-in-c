bbbabae16c9f44c3e47f19a29f704fa863402648
RaselMridha792 <raselmridha792@gmail.com>
2025-04-28 18:28:00 +0600

    solve the float operation problem

--- problem1-for-data-type.c
+++ problem1-for-data-type.c
@@ -3,10 +3,19 @@
 int main () {
 
       // ইউজার থেকে দুইটি পূর্ণসংখ্যা (int) ইনপুট নিয়ে তাদের যোগফল বের করো। 
-      int a, b;
-      printf("enter your numbers: ");
-      scanf("%d %d", &a, &b);
-      int sum = a + b;
-      printf("the sum is:  %d ", sum);
+      // int a, b;
+      // printf("enter your numbers: ");
+      // scanf("%d %d", &a, &b);
+      // int sum = a + b;
+      // printf("the sum is:  %d ", sum);
+
+      // 2. ইউজার থেকে তিনটি ভাসমান সংখ্যা (float) ইনপুট নিয়ে তাদের গড় বের করো।
+      // float a, b, c;
+      // printf("enter your numbers: ");
+      // scanf("%f %f %f", &a, &b, &c);
+      // float avg = (a + b + c) / 3;
+      // printf("the avarage number is: %.1f", avg);
+
+      
     return 0;
 }
